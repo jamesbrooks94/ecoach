c028a40778141195dbd6c62eac3f61feba74e767
James Brooks <james@brookode.uk>
2020-04-03 21:42:26 +0100

    Permissions

--- db/src/index.ts
+++ db/src/index.ts
@@ -1,12 +1,33 @@
 import express from 'express'
-import { postgraphile } from 'postgraphile'
+import { postgraphile, makeProcessSchemaPlugin } from 'postgraphile'
 import { env } from './config'
 import knex from 'knex'
 import config from './knexfile'
 import { createLogger } from './logger'
 import bff from './bff'
+import { shield, rule } from 'graphql-shield'
+import { applyMiddleware } from 'graphql-middleware'
 
 const logger = createLogger('ecoach-db')
+
+export const allow = rule()((_a, _b, { user }, { fieldName }) => {
+  console.log(user, fieldName)
+  return true
+})
+
+const permissions = shield(
+  {
+    Query: {
+      '*': allow,
+    },
+  },
+  {
+    fallbackError: Error('You may not have permissions to perform this action'),
+    allowExternalErrors: true, // Show internal errors
+  }
+)
+
+const plugin = makeProcessSchemaPlugin((schema) => applyMiddleware(schema, permissions))
 
 logger.info('Starting migration')
 const client = knex(config)
@@ -20,10 +41,16 @@ const init = async () => {
 
     db.use(
       postgraphile(env.DB_CONNECTION, 'public', {
+        additionalGraphQLContextFromRequest: async (req, res): Promise<any> => {
+          return {
+            user: 'james',
+          }
+        },
         watchPg: true,
         graphiql: true,
         enhanceGraphiql: true,
         graphiqlRoute: '/',
+        appendPlugins: [plugin],
       })
     )
 
